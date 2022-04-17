dd8a4b35a4082c9daebd4a56072a5a0c93360574
Brian Campbell <bry123ftw@gmail.com>
2022-04-16 23:22:53 -0500

    empty function done

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -11,6 +11,7 @@ function addItem (item){
 console.log("adding item:", addItem(`apples`));
 //console.log(`basket is: ${basket}`);
 console.log("adding item:", addItem(`peanuts`));
+console.log("adding item:", addItem(`cheese`));
 
 function listItems(basket){
     for(i=0; i<basket.length; i++){
@@ -20,5 +21,12 @@ function listItems(basket){
 }
 console.log('items in basket are:', listItems(basket));
 function empty(basket){
-
+    for(i=basket.length; i>0; i--){
+        console.log('item removed:', basket.pop()); //if I want to display what is removed
+        //basket.pop(); //if I don't want to display what is being removed
+    }
+    return true
 }
+console.log('Are items removed from basket?', empty(basket));
+
+console.log('basket is', basket);
