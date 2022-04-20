e668a47909de825ef3e0adb32468299d1624abf1
Brian Campbell <bry123ftw@gmail.com>
2022-04-20 14:40:43 -0500

    added the isFull function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,10 +2,13 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 let basket = [];
+const maxItems = 5;
 
 function addItem (item){
-    basket.push(item);
-    return true; 
+    if (isFull(basket)===false){
+        basket.push(item);
+        return true; 
+    }
 }
 
 console.log("adding item:", addItem(`apples`));
@@ -30,3 +33,21 @@ function empty(basket){
 console.log('Are items removed from basket?', empty(basket));
 
 console.log('basket is', basket);
+
+function isFull(){
+    console.log(basket)
+    if(basket.length < maxItems){
+        return false;
+    }else{
+        return true;
+    }
+}
+addItem('apples');
+addItem('peanuts');
+addItem('pears');
+
+console.log('my basket has 3 items, is it full? ', isFull(basket));
+
+addItem('hamburger');
+addItem('chicken');
+console.log('my basket has 5 items, is it full? ', isFull(basket));
